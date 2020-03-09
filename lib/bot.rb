7244d2f29cfcb1fcaecad5815746e6f53c827090
kip <cyruskiprop254@gmail.com>
2020-03-09 18:00:41 +0300

    add description of all the commands the bot accepts

--- lib/bot.rb
+++ lib/bot.rb
@@ -3,16 +3,25 @@ module GeekyBot
       help do
         title 'Well paid geek toolkit'
         desc 'This is a slack utility bot for a tech savvy. You can access various services i.e 
-           -> getting job updates
-           -> Programming memes
-           -> Inspitional quotes
-           -> Css tricks Articles
-           -> More features we be coming soon
+          -> getting job updates
+          -> Programming memes
+          -> Css tricks Feeds
+          -> Awesome tech related articles
+          -> More features will be coming soon
         .'
   
         command :jobs? do
           title 'jobs?'
           desc 'get the latest ruby jobs feed from stack overflow. Returns all the valid urls of the specific jobs. You can click on the links for more information about the job that interest/suits you'
+
+          title 'articles?' 
+          desc 'get the latest ruby programming articles from hackernoon archives'
+        
+          title 'Hi?'
+          desc 'Greets the bot!!'
+
+          title 'css_tricks?'
+          desc 'offers top notch articles for frontend development including React, CSS/HTML, angular, small css mini projectss etc'
         end
       end
     end
